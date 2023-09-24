75d64068f480e363f226e22ec382ac3dbcc0d970
RNLFoof <z.zablotsky@gmail.com>
2023-09-24 17:20:20 -0400

    feature: value_for_progress

--- aneg/main.py
+++ aneg/main.py
@@ -18,7 +18,19 @@ def generate_emojis(numbers: Sequence[Decimal], output_directory="output") -> st
 
 def value_for_progress(progress: float, number_of_values: int,  value_at_no_progress: float,
                        value_at_max_progress: float) -> float:
-    ...
+    # This is set up in a way that uses the sequence 0, 1, 2, 4... instead of the more consistent 0.5, 1, 2, 4...
+    # But it ensures that both the lowest and highest provided values are both placed somewhere.
+    # Maybe change it someday to use a consistent curve that actually hits both??
+    if progress == 0:
+        return 0
+    progress_delta = value_at_no_progress + value_at_max_progress
+    at_nth_item = int(progress * (number_of_values - 1))
+    return value_at_no_progress + (
+        progress_delta * (
+            (2 ** at_nth_item) /
+            (2 ** (number_of_values - 1))
+        )
+    )
 
 def enumerate_with_progress(numbers: list[float]) -> Generator[tuple[float, float], None, None]:
     ...
